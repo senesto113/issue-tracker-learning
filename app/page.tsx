86628e4d46bd2023894742803169526c2823323e
senesto113 <senesto113@gmail.com>
2025-12-09 15:23:10 +0500

    build the navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,21 @@
-export default function Home() {
+import './globals.css'
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import NavBar from './NavBar'
+
+const inter = Inter({ subsets: ['latin'] })
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
   return (
-    <div>Hello World</div>
-  );
+    <html lang="en">
+      <body className={inter.className}>
+        <NavBar />
+        <h1>hello world</h1>
+        <main>{children}</main>
+      </body>
+    </html>
+  )
 }
